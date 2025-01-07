eda0924f13e3585c1ec9e2c58e93d8e2a5f7b738
hxsggsz <vt.hugo.2021@gmail.com>
2025-01-07 15:48:24 -0300

    add spring security and disable csrf

--- src/main/java/com/br/spring/modules/candidate/controllers/controller.java
+++ src/main/java/com/br/spring/modules/candidate/controllers/controller.java
@@ -1,13 +1,15 @@
 package com.br.spring.modules.candidate.controllers;
 
-
 import com.br.spring.modules.candidate.dto.CreateCandidateDTO;
 import com.br.spring.modules.candidate.useCases.CreateCandidateUseCase;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/candidates")
@@ -18,8 +20,8 @@ public class controller {
     @PostMapping("/")
     public ResponseEntity<Object> create(@Valid @RequestBody CreateCandidateDTO candidate) {
         try {
-        var result = this.createCandidateUseCase.execute(candidate);
-        return ResponseEntity.status(HttpStatus.CREATED).body(result);
+            var result = this.createCandidateUseCase.execute(candidate);
+            return ResponseEntity.status(HttpStatus.CREATED).body(result);
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
         }
